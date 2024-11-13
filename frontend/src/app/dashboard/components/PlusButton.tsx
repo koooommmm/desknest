4d28a7d8d4f51c60e7c3a6e60c65f9277c28330d
koooommmm <nico.pika17@gmail.com>
2024-11-13 21:24:04 +0900

    fix: ボタンのデザインを修正

--- frontend/src/app/dashboard/components/PlusButton.tsx
+++ frontend/src/app/dashboard/components/PlusButton.tsx
@@ -5,7 +5,7 @@ const PlusButton: React.FC = () => {
   return (
     <Link href='/post'>
       <button
-        className='fixed bottom-6 right-6 w-14 h-14 bg-blue-500 text-white rounded-full shadow-lg flex items-center justify-center text-3xl hover:bg-blue-600'
+        className='fixed bottom-6 right-6 w-14 h-14 bg-gray-700 text-white rounded-full shadow-lg flex items-center justify-center text-3xl hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700'
         aria-label='Add Post'
       >
         +
