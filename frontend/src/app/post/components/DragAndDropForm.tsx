a9b398ab00bd4b2c0e930e430e4d9b991e39e387
koooommmm <nico.pika17@gmail.com>
2024-11-13 21:23:45 +0900

    update: 投稿ページで画像を選択できるように変更

--- frontend/src/app/post/components/DragAndDropForm.tsx
+++ frontend/src/app/post/components/DragAndDropForm.tsx
@@ -1,21 +1,15 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import { useDropzone } from 'react-dropzone';
 
 const DragAndDropForm: React.FC = () => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
-    accept: { 'image/*': [] },
+    accept: {
+      'image/*': [],
+    },
   });
 
-  const [description, setDescription] = useState('');
-
-  const handleDescriptionChange = (
-    e: React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    setDescription(e.target.value);
-  };
-
-  const files = acceptedFiles.map((file) => (
+  const images = acceptedFiles.map((file) => (
     <div
       key={file.path}
       className='w-24 h-24 overflow-hidden rounded-lg shadow-lg'
@@ -41,25 +35,10 @@ const DragAndDropForm: React.FC = () => {
           Drag 'n' drop some image files here, or click to select files
         </p>
       </div>
-
-      <textarea
-        value={description}
-        onChange={handleDescriptionChange}
-        placeholder='Enter a description for your post...'
-        className='mt-4 w-full p-3 border border-gray-300 rounded-lg resize-none h-32 focus:outline-none focus:ring-2 focus:ring-gray-300 bg-gray-100 placeholder-gray-500 text-gray-700'
-      />
-
       <aside className='mt-4'>
         <h4 className='text-lg font-semibold text-gray-800 mb-2'>Preview</h4>
-        <div className='flex flex-wrap gap-4'>{files}</div>
+        <div className='flex flex-wrap gap-4'>{images}</div>
       </aside>
-
-      <button
-        type='submit'
-        className='mt-6 w-full py-2 bg-gray-700 text-white rounded-lg shadow-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 transition duration-150'
-      >
-        Post
-      </button>
     </section>
   );
 };
